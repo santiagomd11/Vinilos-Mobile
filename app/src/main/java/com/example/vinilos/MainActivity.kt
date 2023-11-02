18036c8344185d0578d2fbf174e44003ed13c00a
c.cortez <c.cortez@uniandes.edu.co>
2023-11-01 19:42:35 -0500

    Add broker to call api endpoint

--- app/src/main/java/com/example/vinilos/MainActivity.kt
+++ app/src/main/java/com/example/vinilos/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.vinilos
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -11,12 +14,15 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Response
+import com.example.vinilos.brokers.VolleyBroker
 import com.example.vinilos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    lateinit var volleyBroker: VolleyBroker
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +30,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        volleyBroker = VolleyBroker(this.applicationContext)
         setSupportActionBar(binding.appBarMain.toolbar)
+
+        val getButton: Button = findViewById(R.id.test_endpoint_button)
+        val getResultTextView : TextView = findViewById(R.id.get_result_text)
+        getButton.setOnClickListener {
+            volleyBroker.instance.add(VolleyBroker.getRequest("musicians",
+                Response.Listener<String> { response ->
+                    // Display the first 500 characters of the response string.
+                    getResultTextView.text = "Response is: ${response}"
+                },
+                Response.ErrorListener {
+                    Log.d("TAG", it.toString())
+                    getResultTextView.text = "That didn't work!"
+                }
+            ))
+        }
 
         binding.appBarMain.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -42,6 +64,8 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
