28c1c0a38fa98fdc3dd55631bc1863579419e8ed
c.cortez <c.cortez@uniandes.edu.co>
2023-11-15 18:51:19 -0500

    Add datepicker on birthDate

--- app/src/main/java/com/example/vinilos/ui/create_album/CreateAlbumFragment.kt
+++ app/src/main/java/com/example/vinilos/ui/create_album/CreateAlbumFragment.kt
@@ -1,14 +1,25 @@
 package com.example.vinilos.ui.create_album
 
-import androidx.lifecycle.ViewModelProvider
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.vinilos.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.vinilos.databinding.FragmentCreateAlbumBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
 
 class CreateAlbumFragment : Fragment() {
+    private var _binding: FragmentCreateAlbumBinding? = null
+    private val binding get() = _binding!!
+    val myCalendar: Calendar = Calendar.getInstance()
 
     companion object {
         fun newInstance() = CreateAlbumFragment()
@@ -20,13 +31,59 @@ class CreateAlbumFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_create_album, container, false)
+        val createAlbumViewModel =
+            ViewModelProvider(this).get(CreateAlbumViewModel::class.java)
+
+        _binding = FragmentCreateAlbumBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        binding.cancelSaveAlbumButton.setOnClickListener{
+            getActivity()?.onBackPressedDispatcher?.onBackPressed()
+        }
+
+        binding.saveAlbumButton.setOnClickListener {
+            if(TextUtils.isEmpty(binding.textNameField.text.toString())){
+                binding.nameTextInputLayout.error = "El nombre es obligatorio"
+            }
+            else if(TextUtils.isEmpty(binding.textImageField.text.toString())){
+                binding.imageTextInputLayout.error = "La imagen es obligatoria"
+            }
+            else if(TextUtils.isEmpty(binding.textDescriptionField.text.toString())){
+                binding.descriptionTextInputLayout.error = "La descripcion es obligatoria"
+            }
+            else if(TextUtils.isEmpty(binding.textBirthDateField.text.toString())){
+                binding.birthDateTextInputLayout.error = "La fecha de nacimiento es obligatoria"
+            }
+
+        }
+
+        super.onCreate(savedInstanceState)
+        val date =
+            OnDateSetListener { view, year, month, day ->
+                myCalendar[Calendar.YEAR] = year
+                myCalendar[Calendar.MONTH] = month
+                myCalendar[Calendar.DAY_OF_MONTH] = day
+                updateLabel()
+            }
+        binding.textBirthDateField.setOnClickListener(View.OnClickListener {
+            DatePickerDialog(
+                this.context as Context,
+                date,
+                myCalendar[Calendar.YEAR],
+                myCalendar[Calendar.MONTH],
+                myCalendar[Calendar.DAY_OF_MONTH]
+            ).show()
+        })
+
+        return root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(CreateAlbumViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun updateLabel() {
+        val myFormat = "MM/dd/yy"
+        val dateFormat = SimpleDateFormat(myFormat, Locale.US)
+        binding.textBirthDateField.setText(dateFormat.format(myCalendar.time))
     }
+
+
 
 }
